6de602bef581b1f07d64eec1458004fe86f991c1
Geovanny Gonsales <geovanny.gonsales.dev@gmail.com>
2023-08-07 20:55:16 -0300

    refatorado codigo e adicionado o comp menu

---
+++ space/src/componentes/Menu/index.jsx
@@ -0,0 +1,36 @@
+import styles from "./Menu.module.scss";
+import home from "../../assets/icones/home.png";
+import maisCurtidas from "../../assets/icones/mais-curtidas.png";
+import maisVistas from "../../assets/icones/mais-vistas.png";
+import novas from "../../assets/icones/novas.png";
+import surpreendaMe from "../../assets/icones/surpreenda-me.png";
+function Menu() {
+  return (
+    <nav className={styles.menu}>
+      <ul className={styles.menu__lista}>
+        <li className={styles.menu__item}>
+          <img src={home} alt=""></img>
+          <a href="/">Início</a>
+        </li>
+        <li className={styles.menu__item}>
+          <img src={maisCurtidas} alt=""></img>
+          <a href="/">Mais curtidas</a>
+        </li>
+        <li className={styles.menu__item}>
+          <img src={maisVistas} alt=""></img>
+          <a href="/">Mais vistas</a>
+        </li>
+        <li className={styles.menu__item}>
+          <img src={novas} alt=""></img>
+          <a href="/">Nova</a>
+        </li>
+        <li className={styles.menu__item}>
+          <img src={surpreendaMe} alt=""></img>
+          <a href="/">Surpreenda-me</a>
+        </li>
+      </ul>
+    </nav>
+  );
+}
+
+export default Menu;
--- space/src/paginas/PaginaInicial/index.jsx
+++ space/src/paginas/PaginaInicial/index.jsx
@@ -1,9 +1,23 @@
 import Cabecalho from "../../componentes/Cabecalho";
+import Menu from "../../componentes/Menu";
+import styles from "./PaginaInicial.module.scss";
+import banner from "../../assets/icones/banner.png";
 
-function PaginaInicial(){
-    return(
-        <Cabecalho/>
-    );
+function PaginaInicial() {
+  return (
+    <>
+      <Cabecalho />
+      <main>
+        <section className={styles.principal}>
+          <Menu />
+          <div  className={styles.principal__imagem}>
+            <h1>A galeria mais completa do espaço</h1>
+            <img src={banner} alt="A imagem da terra vista do espaço" />
+          </div>
+        </section>
+      </main>
+    </>
+  );
 }
 
 export default PaginaInicial;
